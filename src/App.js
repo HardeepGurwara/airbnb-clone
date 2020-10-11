d1006589a38d1c1d2f005aa3fd949efe310d8f5c
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-10-12 00:27:31 +0100

    Style the footer and is working on search page component, more on the next commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="app">
+      <Router>     
       <Header />
-      <Home />
-      {/* home */}
-      {/* <header */}
-      {/* banner */}
-
-      {/* cards */}
-
-      {/* footer */}
-
-      {/* searchPahe */}
-      {/* ..... */}
+          <Switch> 
+           
+             <Route path="/search" >
+             <SearchPage />
+             </Route>
+             <Route path="/" >
+             <Home />
+             </Route>
+          </Switch>
+      <Footer />
+     </Router>
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SearchPage.css'
+function SearchPage() {
+    return (
+        <div classNam="searchpage"> 
+            <h1>Im the boss</h1>
+        </div>
+    )
+}
+
+export default SearchPage
